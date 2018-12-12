fe81caf63b98d5cc8e69db56ee7caea3641d4f3c
Alejandro <alejandrochang10@gmail.com>
2018-12-11 22:56:38 -0800

    styling components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
+import Table from "./Table";
 
 const App = () => {
-  return(
-    <div>Hello World</div>
+  return (
+    <div>
+      <h1>Schedule of Investments: </h1>
+      <Table />
+    </div>
   );
-}
+};
 
 export default App;
